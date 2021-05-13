515ca52cc95470aee38d3badc91fa7f053938ab6
Gianluca Brilli <gianluca.brilli@unimore.it>
2021-05-13 22:03:14 +0200

    HLS lab 1

---
+++ hls/lab1/exercise_0.hpp
@@ -0,0 +1,11 @@
+#ifndef VADD_HPP
+#define VADD_HPP
+
+	#include <stdio.h>
+
+	//#define TEST_DATA_SIZE 4194304 // 2^22
+	#define TEST_DATA_SIZE 12
+
+	void vadd(int *a, int *b, int *c, const int len);
+
+#endif
--- hls/lab1/exercise_0_solution.cpp
+++ hls/lab1/exercise_0_solution.cpp
@@ -1,12 +1,12 @@
-#define TEST_DATA_SIZE 4194304 // 2^22
+#include "exercise_0.hpp"
 
 const unsigned int c_dim = TEST_DATA_SIZE;
 
 void vadd(int *a, int *b, int *c, const int len)
 {
-    #pragma HLS INTERFACE m_axi port=a offset=slave bundle=mem
-    #pragma HLS INTERFACE m_axi port=b offset=slave bundle=mem
-    #pragma HLS INTERFACE m_axi port=c offset=slave bundle=mem
+    #pragma HLS INTERFACE m_axi port=a offset=slave depth=c_dim bundle=mem
+    #pragma HLS INTERFACE m_axi port=b offset=slave depth=c_dim bundle=mem
+    #pragma HLS INTERFACE m_axi port=c offset=slave depth=c_dim bundle=mem
     #pragma HLS INTERFACE s_axilite port=len bundle=params
     #pragma HLS INTERFACE s_axilite port=return bundle=params
 
